5825c6ff190efc3aa0e2a4246fdeb7a976231229
Johnclinton Luseno <111067111+clintmadeit@users.noreply.github.com>
2023-01-11 20:50:14 +0300

    read-file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
-  
 #!/usr/bin/python3
-"""This module defines a text file-reading function"""
+"""Module for read_file method"""
 
 
 def read_file(filename=""):
-    """Prints the contents of a UTF8 text file"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Method that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as my_file_0:
+        print(my_file_0.read(), end="")
