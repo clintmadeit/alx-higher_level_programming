756b24aadc8521ebc994162e346beba8871c9035
Johnclinton Luseno <111067111+clintmadeit@users.noreply.github.com>
2023-01-11 20:57:16 +0300

    read-file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Method that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as my_file_0:
-        print(my_file_0.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
